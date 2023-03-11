af548ab3eeb3f6682a2e0c75544ceae83ab38ac8
mk.barbara <71386896+mkbarbara@users.noreply.github.com>
2023-03-11 19:41:36 +0300

    Update RegistrationTest.java

--- src/test/java/ru/netology/RegistrationTest.java
+++ src/test/java/ru/netology/RegistrationTest.java
@@ -19,9 +19,9 @@ class RegistrationTest {
         $("[data-test-id = name] input").setValue("Макарова Варвара");
         $("[data-test-id = phone] input").setValue("+79856789456");
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
-        Date dateNow = new Date();
-        SimpleDateFormat formatForDateNow = new SimpleDateFormat("dd.MM.yyyy");
-        $("[data-test-id = date] input").setValue(formatForDateNow.format(dateNow));
+        LocalDate dateNow = LocalDate.now().plusDays(4);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        $("[data-test-id = date] input").setValue(dateNow.format(formatter));
         $("[data-test-id = agreement] [role= presentation]").click();
         $$("button").find(exactText("Забронировать")).click();
         $(withText("Успешно!")).shouldBe(visible, Duration.ofMillis(15000));
